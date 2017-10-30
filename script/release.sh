60e52f2f7b6c1dd41b8196996e32391dc7f516d8
Beth Skurrie <beth@bethesque.com>
2017-10-30 20:08:53 +1100

    chore: remove gemfiles from file to be committed when releasing

--- script/release.sh
+++ script/release.sh
@@ -2,6 +2,6 @@
 set -e
 bundle exec rake
 bundle exec rake generate_changelog
-git add CHANGELOG.md lib/pact_broker/client/version.rb gemfiles
+git add CHANGELOG.md lib/pact_broker/client/version.rb
 git commit -m "Releasing version $(ruby -r ./lib/pact_broker/client/version.rb -e "puts PactBroker::Client::VERSION")"
 bundle exec rake release
