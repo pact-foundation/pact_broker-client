607324e0795204267f245206e2cee6047d5a0b73
Beth Skurrie <beth@bethesque.com>
2017-10-30 20:07:31 +1100

    chore: remove appraisal update from release script

--- script/release.sh
+++ script/release.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 bundle exec rake
-bundle exec appraisal update
 bundle exec rake generate_changelog
 git add CHANGELOG.md lib/pact_broker/client/version.rb gemfiles
 git commit -m "Releasing version $(ruby -r ./lib/pact_broker/client/version.rb -e "puts PactBroker::Client::VERSION")"
