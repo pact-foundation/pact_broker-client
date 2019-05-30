d67d6e96bf7966c0a85d1cf7e446313509e88496
Beth Skurrie <beth@bethesque.com>
2019-05-30 17:27:26 +1000

    docs: record git env vars for various ci software

--- lib/pact_broker/client/git.rb
+++ lib/pact_broker/client/git.rb
@@ -1,5 +1,13 @@
 require 'pact_broker/client/error'
 
+ # BUILDKITE_BRANCH BUILDKITE_COMMIT https://buildkite.com/docs/pipelines/environment-variables
+ # CIRCLE_BRANCH CIRCLE_SHA1 https://circleci.com/docs/2.0/env-vars/
+ # TRAVIS_COMMIT TRAVIS_BRANCH - TRAVIS_PULL_REQUEST_BRANCH TRAVIS_PULL_REQUEST_SHA https://docs.travis-ci.com/user/environment-variables/
+ # GIT_COMMIT GIT_BRANCH https://wiki.jenkins.io/display/JENKINS/Building+a+software+project
+ # GIT_COMMIT GIT_LOCAL_BRANCH https://hudson.eclipse.org/webtools/env-vars.html/
+ # APPVEYOR_REPO_COMMIT APPVEYOR_REPO_BRANCH       https://www.appveyor.com/docs/environment-variables/
+ # bamboo.repository.git.branch https://confluence.atlassian.com/bamboo/bamboo-variables-289277087.html
+
 module PactBroker
   module Client
     module Git
