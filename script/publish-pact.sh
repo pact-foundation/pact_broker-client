341ed9615e5d5e38d8c935a0538e99d414b3a65f
Beth Skurrie <beth@bethesque.com>
2019-05-30 17:27:46 +1000

    chore: update script/publish-pact.sh
    [ci skip]

--- script/publish-pact.sh
+++ script/publish-pact.sh
@@ -1,1 +1,1 @@
-bundle exec bin/pact-broker publish spec/pacts/pact_broker_client-pact_broker.json --consumer-app-version 1.2.3 --broker-base-url http://localhost:9292 --tag-with-git-branch
+bundle exec bin/pact-broker publish spec/pacts/pact_broker_client-pact_broker.json --consumer-app-version 1.2.3 --broker-base-url http://localhost:9292 --tag-with-git-branch --broker-username localhost --broker-password localhost
