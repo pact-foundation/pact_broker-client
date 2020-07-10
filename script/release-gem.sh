2f182ae8cdae2672350fae23aec9c9ba2aee9b3d
Beth Skurrie <beth@bethesque.com>
2020-07-11 07:34:09 +1000

    chore: update release workflow

--- script/release-gem.sh
+++ script/release-gem.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-if [ -n "${GITHUB_ACTIONS}" ]; then
-  : "${RUBYGEMS_API_KEY:?RUBYGEMS_API_KEY must be set}"
-  : "${GITHUB_TOKEN:?GITHUB_TOKEN must be set}"
+# if [ -n "${GITHUB_ACTIONS}" ]; then
+#   : "${RUBYGEMS_API_KEY:?RUBYGEMS_API_KEY must be set}"
+#   : "${GITHUB_TOKEN:?GITHUB_TOKEN must be set}"
 
-  echo "Setting up gem credentials..."
-  set +x
-  mkdir -p ~/.gem
+#   echo "Setting up gem credentials..."
+#   set +x
+#   mkdir -p ~/.gem
 
-  cat << EOF > ~/.gem/credentials
-  ---
-  :github: Bearer ${GITHUB_TOKEN}
-  :rubygems_api_key: ${RUBYGEMS_API_KEY}
-EOF
+#   cat << EOF > ~/.gem/credentials
+#   ---
+#   :github: Bearer ${GITHUB_TOKEN}
+#   :rubygems_api_key: ${RUBYGEMS_API_KEY}
+# EOF
 
-  chmod 0600 ~/.gem/credentials
-  set -x
+#   chmod 0600 ~/.gem/credentials
+#   set -x
 
-fi
+# fi
 
 echo "Running gem release task..."
 bundle exec rake release
