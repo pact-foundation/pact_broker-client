b015e1e73b3dabf85bc926f75b2ac9d669e4952c
Beth Skurrie <beth@bethesque.com>
2020-07-10 18:36:34 +1000

    chore: disable rake while develping release workflow

--- script/release.sh
+++ script/release.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-bundle exec rake
+# bundle exec rake
 bundle exec bump ${1:-minor} --no-commit
 bundle exec rake generate_changelog
 git add CHANGELOG.md lib/pact_broker/client/version.rb
