1fabb8b2d4b0512c7dba9e9a398c5d2e20300b45
Beth Skurrie <beth@bethesque.com>
2020-07-11 09:50:20 +1000

    fix: not a real fix, just testing release notes

--- script/prepare-github-release.sh
+++ script/prepare-github-release.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-tag=$(cat CHANGELOG.md  | grep "#" | head -n 1 | cut -d' ' -f2)
+tag=$(git describe)
 mkdir -p tmp
 git diff CHANGELOG.md | grep "^+" | grep -v "b/CHANGELOG.md" | sed 's/^+//g' > tmp/RELEASE_NOTES.md
 echo "::set-env name=TAG::${tag}"
