94ab848cdb1f89e60fd0fe63f92e10424ec98dbd
Beth Skurrie <beth@bethesque.com>
2020-07-13 13:47:41 +1000

    chore: update release scripts

--- script/trigger-release.sh
+++ script/trigger-release.sh
@@ -1,14 +1,26 @@
-#!/bin/bash
+#!/bin/sh
 
-output=$(curl -v -X POST https://api.github.com/repos/pact-foundation/pact_broker-client/dispatches \
-      -H 'Accept: application/vnd.github.everest-preview+json' \
-      -H "Authorization: Bearer $GITHUB_ACCESS_TOKEN" \
-      -d '{"event_type": "release-patch"}' 2>&1)
+: "${GITHUB_ACCESS_TOKEN_FOR_PF_RELEASES:?Please set environment variable GITHUB_ACCESS_TOKEN_FOR_PF_RELEASES}"
 
-if  ! echo "${output}" | grep "HTTP\/1.1 204" > /dev/null; then
-  echo "$output" | sed  "s/${GITHUB_ACCESS_TOKEN}/****/g"
-  echo "Failed to do the thing"
-  exit 1
+if [ -n "$1" ]; then
+  increment="\"${1}\""
+else
+  increment="null"
 fi
 
-echo "See https://github.com/pact-foundation/pact_broker-client/actions?query=workflow%3A%22Release+gem%22"
+repository_slug=$(git remote get-url $(git remote show) | cut -d':' -f2 | sed 's/\.git//')
+
+output=$(curl -v -X POST https://api.github.com/repos/${repository_slug}/dispatches \
+      -H 'Accept: application/vnd.github.everest-preview+json' \
+      -H "Authorization: Bearer $GITHUB_ACCESS_TOKEN_FOR_PF_RELEASES" \
+      -d "{\"event_type\": \"release\", \"client_payload\": {\"increment\": ${increment}}}" 2>&1)
+
+if  ! echo "${output}" | grep "HTTP\/1.1 204" > /dev/null; then
+  echo "$output" | sed  "s/${GITHUB_ACCESS_TOKEN_FOR_PF_RELEASES}/********/g"
+  echo "Failed to trigger release"
+  exit 1
+else
+  echo "Release workflow triggered"
+fi
+
+echo "See https://github.com/${repository_slug}/actions?query=workflow%3A%22Release+gem%22"
