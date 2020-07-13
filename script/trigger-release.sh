9e8cc0d4afb0206d5b1bd769b14aa83e1a4fdba3
Beth Skurrie <beth@bethesque.com>
2020-07-13 13:58:38 +1000

    chore: update release scripts

--- script/trigger-release.sh
+++ script/trigger-release.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Script to trigger release of gem via the pact-foundation/release-gem action
+# Requires a Github API token with repo scope stored in the
+# environment variable GITHUB_ACCESS_TOKEN_FOR_PF_RELEASES
+
 : "${GITHUB_ACCESS_TOKEN_FOR_PF_RELEASES:?Please set environment variable GITHUB_ACCESS_TOKEN_FOR_PF_RELEASES}"
 
 if [ -n "$1" ]; then
@@ -13,7 +17,7 @@ repository_slug=$(git remote get-url $(git remote show) | cut -d':' -f2 | sed 's
 output=$(curl -v -X POST https://api.github.com/repos/${repository_slug}/dispatches \
       -H 'Accept: application/vnd.github.everest-preview+json' \
       -H "Authorization: Bearer $GITHUB_ACCESS_TOKEN_FOR_PF_RELEASES" \
-      -d "{\"event_type\": \"release\", \"client_payload\": {\"increment\": ${increment}}}" 2>&1)
+      -d "{\"event_type\": \"release-triggered\", \"client_payload\": {\"increment\": ${increment}}}" 2>&1)
 
 if  ! echo "${output}" | grep "HTTP\/1.1 204" > /dev/null; then
   echo "$output" | sed  "s/${GITHUB_ACCESS_TOKEN_FOR_PF_RELEASES}/********/g"
