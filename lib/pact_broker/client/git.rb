24d800f4f26353b9c81ad6af044348e0d07a9c5d
Beth Skurrie <beth@bethesque.com>
2020-07-18 18:24:33 +1000

    fix: testing release process

--- lib/pact_broker/client/git.rb
+++ lib/pact_broker/client/git.rb
@@ -11,7 +11,7 @@ APPVEYOR_REPO_COMMIT APPVEYOR_REPO_BRANCH       https://www.appveyor.com/docs/en
 CI_COMMIT_REF_NAME https://docs.gitlab.com/ee/ci/variables/predefined_variables.html
 CI_BRANCH CI_COMMIT_ID https://documentation.codeship.com/pro/builds-and-configuration/environment-variables/
 bamboo.repository.git.branch https://confluence.atlassian.com/bamboo/bamboo-variables-289277087.html
-BITBUCKET_BRANCH https://confluence.atlassian.com/bitbucket/variables-in-pipelines-794502608.html
+BITBUCKET_BRANCH BITBUCKET_COMMIT https://confluence.atlassian.com/bitbucket/variables-in-pipelines-794502608.html
 
 =end
 
@@ -21,6 +21,11 @@ module PactBroker
     module Git
       COMMAND = 'git name-rev --name-only HEAD'.freeze
       BRANCH_ENV_VAR_NAMES = %w{BUILDKITE_BRANCH CIRCLE_BRANCH TRAVIS_BRANCH GIT_BRANCH GIT_LOCAL_BRANCH APPVEYOR_REPO_BRANCH CI_COMMIT_REF_NAME BITBUCKET_BRANCH}.freeze
+      COMMIT_ENV_VAR_NAMES = %w{BUILDKITE_COMMIT CIRCLE_SHA1 TRAVIS_COMMIT GIT_COMMIT APPVEYOR_REPO_COMMIT CI_COMMIT_ID BITBUCKET_COMMIT}
+
+      def self.commit
+        find_commit_from_env_vars
+      end
 
       def self.branch
         find_branch_from_env_vars || branch_from_git_command
@@ -28,11 +33,15 @@ module PactBroker
 
       # private
 
-      def self.find_branch_from_env_vars
-        BRANCH_ENV_VAR_NAMES.collect { |env_var_name| branch_from_env_var(env_var_name) }.compact.first
+      def self.find_commit_from_env_vars
+        COMMIT_ENV_VAR_NAMES.collect { |env_var_name| value_from_env_var(env_var_name) }.compact.first
       end
 
-      def self.branch_from_env_var(env_var_name)
+      def self.find_branch_from_env_vars
+        BRANCH_ENV_VAR_NAMES.collect { |env_var_name| value_from_env_var(env_var_name) }.compact.first
+      end
+
+      def self.value_from_env_var(env_var_name)
         val = ENV[env_var_name]
         if val && val.strip.size > 0
           val
