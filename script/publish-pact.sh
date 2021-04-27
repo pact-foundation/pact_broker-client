2831c951b3322b036c9578f46ebe0f7c4106d6cf
Beth Skurrie <beth@bethesque.com>
2021-04-27 16:13:15 +1000

    chore: feature flag use of the new publish contracts endpoint

--- script/publish-pact.sh
+++ script/publish-pact.sh
@@ -4,17 +4,18 @@
 #   --description "foo webhook" \
 #   --contract-published
 
-bundle exec bin/pact-broker create-or-update-webhook http://localhost:9393 \
-  --uuid d40f38c3-aaa3-47f5-9161-95c07bc16555 \
-  --provider Bar \
-  --request POST \
-  --contract-published
+# bundle exec bin/pact-broker create-or-update-webhook http://localhost:9393 \
+#   --uuid d40f38c3-aaa3-47f5-9161-95c07bc16555 \
+#   --provider Bar \
+#   --request POST \
+#   --contract-published
 
-bundle exec bin/pact-broker publish spec/pacts/pact_broker_client-pact_broker.json spec/pacts/foo-bar.json \
-  --consumer-app-version 1.2.12 \
+
+PACT_BROKER_FEATURES=publish_contracts bundle exec bin/pact-broker publish spec/pacts/pact_broker_client-pact_broker.json \
+  --consumer-app-version 1.2.26 \
   --broker-base-url http://localhost:9292 \
-   --broker-username localhost --broker-password localhost \
+   --broker-token localhost \
     --auto-detect-version-properties \
     --build-url http://mybuild \
-    --branch master --tag foo5
+    --branch master --tag foo5 --tag foo6
 
