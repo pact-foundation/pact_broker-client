f4da87e9e243bc3ba61aae577b4059ac316a8b11
Beth Skurrie <beth@bethesque.com>
2021-08-07 13:55:18 +1000

    chore: update script [ci-skip]

--- example/scripts/verify-changed-pact.sh
+++ example/scripts/verify-changed-pact.sh
@@ -11,9 +11,12 @@ PACT_CLI_VERSION="0.47.0.0"
 : "${CONSUMER_NAME:?CONSUMER_NAME is not set, cannot check if verification is required.}"
 : "${CONSUMER_VERSION_NUMBER:?CONSUMER_VERSION_NUMBER is not set, cannot check if verification is required.}"
 
+# Delete unnecessary username/password/token
 if docker run --rm \
-    -e PACT_BROKER_BASE_URL=$PACT_BROKER_BASE_URL \
-    -e PACT_BROKER_TOKEN=$PACT_BROKER_TOKEN \
+    -e PACT_BROKER_BASE_URL \
+    -e PACT_BROKER_TOKEN \
+    -e PACT_BROKER_USERNAME \
+    -e PACT_BROKER_PASSWORD \
     -e PACT_BROKER_FEATURES="verification_required" \
     -it \
     pactfoundation/pact-cli:$PACT_CLI_VERSION \
