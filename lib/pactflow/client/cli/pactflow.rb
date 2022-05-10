2d73d7e1f609181e09e701cdf607e7d283fc1a45
Beth Skurrie <beth@bethesque.com>
2022-05-10 12:53:06 +1000

    chore: don't include the broker commands yet

--- lib/pactflow/client/cli/pactflow.rb
+++ lib/pactflow/client/cli/pactflow.rb
@@ -1,10 +1,10 @@
-require "pact_broker/client/cli/broker"
 require "pactflow/client/cli/provider_contract_commands"
+require "pact_broker/client/cli/custom_thor"
 
 module Pactflow
   module Client
     module CLI
-      class Pactflow < PactBroker::Client::CLI::Broker
+      class Pactflow < PactBroker::Client::CLI::CustomThor
         include ::Pactflow::Client::CLI::ProviderContractCommands
       end
     end
