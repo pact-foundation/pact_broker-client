90e8f51f44682b84af78d374d403257765fcb652
Yousaf Nabi <you@saf.dev>
2023-05-04 17:37:04 +0100

    feat: colour first url in magenta for ease of reading

--- lib/pact_broker/client/colorize_notices.rb
+++ lib/pact_broker/client/colorize_notices.rb
@@ -1,5 +1,5 @@
 require 'term/ansicolor'
-
+require "uri"
 module PactBroker
   module Client
     class ColorizeNotices
@@ -11,16 +11,25 @@ module PactBroker
 
       def self.colorized_message(notice)
         color = color_for_type(notice.type)
-        if color
+        uri_string = ::URI.extract(notice.text, %w(http https))
+        if color && uri_string.count >= 1
+          color_for_url(::Term::ANSIColor.color(color, notice.text || ''), uri_string)
+        elsif color
           ::Term::ANSIColor.color(color, notice.text || '')
+        elsif uri_string.count >= 1
+          color_for_url(notice.text, uri_string)
         else
           notice.text
         end
       end
 
+      def self.color_for_url(text, uris)
+        text.gsub!(uris.first, ::Term::ANSIColor.magenta(uris.first))
+      end
+
       def self.color_for_type(type)
         case type
-        when "warning", "prompt" then "yellow"
+        when "warning", "prompt" then :yellow
         when "error", "danger" then :red
         when "success" then :green
         else nil
